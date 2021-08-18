84ad1104ee16d01f7f3567f11a2bf8dc4f92478a
eehaemii <eehaemii@example.com>
2021-08-18 10:50:39 +0900

    header / footer 분리

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,21 @@
+// header / footer 분리 
+window.addEventListener('load', function() {
+    var allElements = document.getElementsByTagName('*');
+    Array.prototype.forEach.call(allElements, function(el) {
+        var includePath = el.dataset.includePath;
+        if (includePath) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    el.outerHTML = this.responseText;
+                }
+            };
+            xhttp.open('GET', includePath, true);
+            xhttp.send();
+        }
+    });
+});
+
 $(function(){
     $(document).on('click', '.menu-btn', function(){
         if($(this).hasClass('active')){
